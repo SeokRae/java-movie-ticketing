9219afe70e52c585f5d1b30b655d2eddabce408e
seokrae <kslbsh@gmail.com>
2022-10-18 21:20:40 +0900

    feat: 영화 예매를 위한 도메인 설계 - Movie 인스턴스 생성 및 가격 확인

--- src/main/java/com/example/ticketing/movie/Movie.java
+++ src/main/java/com/example/ticketing/movie/Movie.java
@@ -1,6 +1,43 @@
 package com.example.ticketing.movie;
 
+import java.util.Objects;
+
+/**
+ * 영화 정보 클래스
+ * 타이틀, 가격 정보를 갖는다.
+ *
+ */
 public class Movie {
+	private final String title;
 	private Money price;
 	
+	public Movie(String title, Money price) {
+		this.title = title;
+		this.price = price;
+	}
+	
+	public Money getPrice() {
+		return price;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Movie)) return false;
+		Movie movie = (Movie) o;
+		return Objects.equals(title, movie.title);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(title);
+	}
+	
+	@Override
+	public String toString() {
+		return "Movie{" +
+			"title='" + title + '\'' +
+			", price=" + price +
+			'}';
+	}
 }
---
+++ src/test/java/com/example/ticketing/movie/MovieTest.java
@@ -0,0 +1,34 @@
+package com.example.ticketing.movie;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("영화 관련 테스트 모음")
+class MovieTest {
+	
+	@DisplayName("영화 생성 테스트(가격은 변동이 가능하다.)")
+	@Test
+	void testCase1() {
+		// given
+		String title = "어벤져스";
+		int amount = 10000;
+		// when
+		Movie movie = new Movie(title, Money.wons(amount));
+		// then
+		assertThat(movie).isEqualTo(new Movie(title, null));
+	}
+	
+	@DisplayName("영화 가격 확인 테스트")
+	@Test
+	void testCase2() {
+		// given
+		String title = "어벤져스";
+		int amount = 10000;
+		// when
+		Movie movie = new Movie(title, Money.wons(amount));
+		// then
+		assertThat(movie.getPrice()).isEqualTo(Money.wons(amount));
+	}
+}
